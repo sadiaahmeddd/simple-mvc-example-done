6642fbb371ded3f31d1c21a1dcfefe32861207a6
ahme0543 <ahme0543@algonquinlive.com>
2025-11-05 17:47:16 -0500

    page 4

--- server/router.js
+++ server/router.js
@@ -1,4 +1,4 @@
-// import the controllers
+/*// import the controllers
 // This only specifies the folder name, which means it will automatically pull the index.js file
 const controllers = require('./controllers');
 
@@ -54,4 +54,38 @@ app.get('/page4', controllers.Dog.listDogs);
 
 
 
+module.exports = router;
+*/ 
+
+
+// server/router.js
+// import the controllers (this loads controllers/index.js)
+const controllers = require('./controllers');
+
+// function to attach routes
+const router = (app) => {
+  // --- existing demo routes ---
+  app.get('/page1', controllers.page1);
+  app.get('/page2', controllers.page2);
+  app.get('/page3', controllers.page3);
+  app.get('/getName', controllers.getName);
+  app.get('/findByName', controllers.searchName);
+
+  // --- Dog routes (add BEFORE the catch-all) ---
+  app.post('/dogs/create', controllers.Dog.createDog);
+  app.post('/dogs/increment', controllers.Dog.incrementAgeByName);
+  app.get('/page4', controllers.Dog.listDogs);
+
+  // --- home ---
+  app.get('/', controllers.index);
+
+  // --- POST routes ---
+  app.post('/setName', controllers.setName);
+  app.post('/updateLast', controllers.updateLast);
+
+  // --- catch-all 404 (keep LAST) ---
+  app.get('*', controllers.notFound);
+};
+
+// export the router function
 module.exports = router;
